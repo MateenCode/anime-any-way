d1330673806c047690227f5d3e512051185b0ca2
Shifath Salam <sheef@Shifaths-MacBook-Pro.local>
2019-03-11 17:49:46 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./css/App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">app</div>;
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <Provider>
+            <Navbar />
+            <Route path="/" component={Home} exact />
+            {/* <Route path="/edit/:id" component={} /> */}
+          </Provider>
+        </React.Fragment>
+      </BrowserRouter>
+    );
   }
 }
 
