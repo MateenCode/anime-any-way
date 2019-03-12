8c8e565884a97bb5e5d88a9c7f2523d124c02854
Mateen Kazia <mateencode@gmail.com>
2019-03-11 18:07:19 -0700

    axios and bootstrap set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "./context/context";
+
 import "./css/App.css";
+
+// components
+import Home from "./components/homepage/home";
+import Navbar from "./components/layout/NavBar";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
+          <Navbar />
           <Provider>
-            <Navbar />
             <Route path="/" component={Home} exact />
             {/* <Route path="/edit/:id" component={} /> */}
           </Provider>
